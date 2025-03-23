d74ccccfaf84bfb62105eb7dbab93681b20be42a
Norberto Jorge Rodriguez Vaquero <norberto.rodriguezvaquero@gmail.com>
2025-03-23 11:03:59 -0300

    Pruebas sobre la agenda.

--- src/principal/Punto3.java
+++ src/principal/Punto3.java
@@ -7,7 +7,8 @@ import trabajoPractico1.ListaEnlazadaContactos;
 public class Punto3 {
 
 	public static void main(String[] args) {
-		Agenda agenda1 = Agenda.crearAgenda();	// creo una agenda
+		// Creo una agenda
+		Agenda agenda1 = Agenda.crearAgenda();
 		
 		// Creación de contactos
 		Contacto c1 = new Contacto("Norberto", "3815566098", "norberto.rodriguezvaquero@gmail.com");
@@ -15,22 +16,41 @@ public class Punto3 {
 		Contacto c3 = new Contacto("Norberto", "3815566098", "norberto.rodriguezvaquero@gmail.com");
 		
 		// agrego los contactos a la agenda
+		System.out.println("Agrego contactos a la agenda: ");
 		agenda1.agregar(c1);
 		agenda1.agregar(c2);
 		agenda1.agregar(c3);
 		
 		agenda1.listar();
 
+		// Buscar por nombre
+		System.out.println("Buscar contacto por nombre 'Ernesto': ");
+		System.out.println("\n" + agenda1.buscar("Ernesto"));
+		
+		System.out.println("\nContactos en la agenda: ");
+		agenda1.listar();
+		System.out.println("Hay " + agenda1.cantidad() + " contactos en la agenda.");
+		
+		// Borrar por número de teléfono
+		System.out.println("\nBorrar contacto por el número de teléfono (3815566098): ");
+		agenda1.borrar("3815566098");
+		agenda1.listar();
+		System.out.println("Hay " + agenda1.cantidad() + " contactos en la agenda.");
+		
+		System.out.println("\nBorrar contacto por el número de teléfono (3815562348): ");
+		agenda1.borrar("3815562348");
+		agenda1.listar();
+		System.out.println("Hay " + agenda1.cantidad() + " contactos en la agenda.");
 		
 	}
 	
-	public Agenda combinarAgendas(Agenda agendaOrigen, Agenda agendaObjetivo) {
-		ListaEnlazadaContactos listaOrigen = agendaOrigen.getListaAgenda();
-		ListaEnlazadaContactos listaObjetivo = agendaObjetivo.getListaAgenda();
-		
-		
-		
-		return null;
-	}
+//	public Agenda combinarAgendas(Agenda agendaOrigen, Agenda agendaObjetivo) {
+//		ListaEnlazadaContactos listaOrigen = agendaOrigen.getListaAgenda();
+//		ListaEnlazadaContactos listaObjetivo = agendaObjetivo.getListaAgenda();
+//		
+//		
+//		
+//		return null;
+//	}
 
 }
