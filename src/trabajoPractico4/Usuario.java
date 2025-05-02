de1dd3a79b40a32362c03e0b1c07cba93f19891f
Norberto Jorge Rodriguez Vaquero <norberto.rodriguezvaquero@gmail.com>
2025-05-02 09:51:49 -0300

    Constructor para Usuario

--- src/trabajoPractico4/Usuario.java
+++ src/trabajoPractico4/Usuario.java
@@ -13,6 +13,11 @@ import java.util.Objects;
 public class Usuario {
     private String nombre;
     private String apellido;
+    
+    public Usuario(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
 
     @Override
     public int hashCode() {
